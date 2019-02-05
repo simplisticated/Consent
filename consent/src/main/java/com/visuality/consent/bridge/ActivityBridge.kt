be235f926837717ffa1158958948155eafc80781
Igor Matyushkin <igormatyushkin014@gmail.com>
2019-02-05 20:10:56 +0300

    Implemented activity bridge

--- consent/src/main/java/com/visuality/consent/bridge/ActivityBridge.kt
+++ consent/src/main/java/com/visuality/consent/bridge/ActivityBridge.kt
@@ -1,8 +1,13 @@
 package com.visuality.consent.bridge
 
 import android.app.Activity
+import android.content.pm.PackageManager
 import com.visuality.consent.check.CheckOperation
+import com.visuality.consent.request.OnRequestOperationFinishedCallback
+import com.visuality.consent.request.RequestHolder
 import com.visuality.consent.request.RequestOperation
+import com.visuality.consent.request.RequestResult
+import kotlin.math.min
 
 fun Activity.checkConsent(
     vararg permissions: String
@@ -19,11 +24,61 @@ fun Activity.checkConsent(
 fun Activity.getConsent(
     vararg permissions: String
 ): RequestOperation {
-    return RequestOperation(
+    val requestOperation = RequestOperation(
         permissions,
         this,
         null
     ).also { operation ->
         operation.start()
     }
+    RequestHolder.currentRequestOperation = requestOperation
+    return requestOperation
+}
+
+fun Activity.handleConsent(
+    requestCode: Int,
+    permissions: Array<out String>,
+    results: IntArray
+): Boolean {
+    val requestOperation = RequestHolder.currentRequestOperation ?: return false
+    val callback = requestOperation.onFinishedCallback ?: return false
+
+    if (requestCode != RequestOperation.REQUEST_CODE) {
+        return false
+    }
+
+    val allowedPermissions = arrayListOf<String>()
+    var blockedPermissions = arrayListOf<String>()
+
+    val endIndex = min(
+        permissions.size,
+        results.size
+    ) - 1
+
+    for (i in 0..endIndex) {
+        val permission = permissions[i]
+        val result = results[i]
+        val permissionAllowed = result == PackageManager.PERMISSION_GRANTED
+
+        if (permissionAllowed) {
+            allowedPermissions.add(permission)
+        } else {
+            blockedPermissions.add(permission)
+        }
+    }
+
+    val requestResult = RequestResult(
+        allowedPermissions.toTypedArray(),
+        blockedPermissions.toTypedArray()
+    )
+    callback(requestResult)
+
+    RequestHolder.currentRequestOperation = null
+    return true
+}
+
+fun Activity.resetConsent() {
+    val requestOperation = RequestHolder.currentRequestOperation ?: return
+    requestOperation.onFinishedCallback = null
+    RequestHolder.currentRequestOperation = null
 }
