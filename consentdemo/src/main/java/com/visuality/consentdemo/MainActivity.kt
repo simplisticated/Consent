8ea204b9786e463db107da35dd8dc6db1f883aff
Igor Matyushkin <igormatyushkin014@gmail.com>
2019-02-05 15:35:29 +0300

    Updated demo project

--- consentdemo/src/main/java/com/visuality/consentdemo/MainActivity.kt
+++ consentdemo/src/main/java/com/visuality/consentdemo/MainActivity.kt
@@ -2,19 +2,29 @@ package com.visuality.consentdemo
 
 import android.Manifest
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.visuality.consent.bridge.getConsent
 
 class MainActivity : AppCompatActivity() {
 
+    private val requestButton by lazy {
+        this.findViewById<Button>(R.id.request_button)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         this.setContentView(R.layout.activity_main)
+        this.initializeRequestButton()
+    }
 
-        this.getConsent(
-            Manifest.permission.CAMERA,
-            Manifest.permission.CALL_PHONE
-        ).onFinished {
+    private fun initializeRequestButton() {
+        this.requestButton.setOnClickListener {
+            this.getConsent(
+                Manifest.permission.CAMERA,
+                Manifest.permission.CALL_PHONE
+            ).onFinished {
+            }
         }
     }
 }
