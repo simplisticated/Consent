70a944e85d9c88cc85f75b1c869fef45a964ab15
Igor Matyushkin <igormatyushkin014@gmail.com>
2019-02-12 17:40:59 +0300

    Updated demo project

--- consentdemo/src/main/java/com/visuality/consentdemo/MainActivity.kt
+++ consentdemo/src/main/java/com/visuality/consentdemo/MainActivity.kt
@@ -1,22 +1,29 @@
 package com.visuality.consentdemo
 
-import android.Manifest
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.visuality.consent.bridge.checkConsent
 import com.visuality.consent.bridge.getConsent
 import com.visuality.consent.bridge.handleConsent
+import com.visuality.consent.types.Permission
 
 class MainActivity : AppCompatActivity() {
 
-    private val requestButton by lazy {
-        this.findViewById<Button>(R.id.request_button)
+    private val checkPermissionsButton by lazy {
+        this.findViewById<Button>(R.id.check_permissions_button)
+    }
+
+    private val requestPermissionsButton by lazy {
+        this.findViewById<Button>(R.id.request_permissions_button)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         this.setContentView(R.layout.activity_main)
-        this.initializeRequestButton()
+        this.prepareCheckPermissionsButton()
+        this.prepareRequestPermissionsButton()
     }
 
     override fun onRequestPermissionsResult(
@@ -31,17 +38,35 @@ class MainActivity : AppCompatActivity() {
         )
     }
 
-    private fun initializeRequestButton() {
-        this.requestButton.setOnClickListener {
+    private fun prepareCheckPermissionsButton() {
+        this.checkPermissionsButton.setOnClickListener {
+            val allowedAccess = !this.checkConsent(
+                Permission.CAMERA,
+                Permission.WRITE_EXTERNAL_STORAGE
+            ).result.hasBlocked
+            val text = if (allowedAccess) "All permissions are available"
+                else "Some permissions are blocked"
+            Toast.makeText(
+                this,
+                text,
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    private fun prepareRequestPermissionsButton() {
+        this.requestPermissionsButton.setOnClickListener {
             this.getConsent(
-                Manifest.permission.CAMERA,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE
-            ).whenFinished { requestResult ->
-                if (requestResult.hasBlocked) {
-                    // Some permissions are blocked
-                } else {
-                    // All permissions are available
-                }
+                Permission.CAMERA,
+                Permission.WRITE_EXTERNAL_STORAGE
+            ).whenFinished { result ->
+                val text = if (result.hasBlocked) "${result.blocked.size} permissions blocked"
+                    else "All permissions are available"
+                Toast.makeText(
+                    this,
+                    text,
+                    Toast.LENGTH_LONG
+                ).show()
             }
         }
     }
