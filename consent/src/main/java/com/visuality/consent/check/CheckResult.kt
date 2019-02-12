1ecb135e4ac3e455a8cd6e33034f6c78a352611b
Igor Matyushkin <igormatyushkin014@gmail.com>
2019-02-12 13:26:58 +0300

    Added internal method to generate empty CheckResult

--- consent/src/main/java/com/visuality/consent/check/CheckResult.kt
+++ consent/src/main/java/com/visuality/consent/check/CheckResult.kt
@@ -1,10 +1,17 @@
 package com.visuality.consent.check
 
-data class CheckResult(
+data class CheckResult internal constructor(
     val allowed: Array<out String>,
     val blocked: Array<out String>
 ) {
 
     val hasBlocked: Boolean
         get() = this.blocked.size > 0
+
+    internal companion object {
+        internal fun empty() = CheckResult(
+            emptyArray(),
+            emptyArray()
+        )
+    }
 }
