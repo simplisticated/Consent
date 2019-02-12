c7461c92ba7c7338ac2d1c0397d9d1bb94a122fb
Igor Matyushkin <igormatyushkin014@gmail.com>
2019-02-12 18:37:33 +0300

    Implemented getConsent() for permission bridge

--- consent/src/main/java/com/visuality/consent/bridge/PermissionBridge.kt
+++ consent/src/main/java/com/visuality/consent/bridge/PermissionBridge.kt
@@ -1,15 +1,35 @@
 package com.visuality.consent.bridge
 
+import android.app.Activity
 import android.content.Context
 import com.visuality.consent.check.CheckOperation
+import com.visuality.consent.request.RequestOperation
 import com.visuality.consent.types.Permission
 
 fun Permission.checkConsent(
     context: Context
 ): CheckOperation {
+    val stringPermissions = arrayOf(
+        this.identifier
+    )
     return CheckOperation(
-        arrayOf(this),
+        stringPermissions,
         context,
+        null
+    ).also { operation ->
+        operation.start()
+    }
+}
+
+fun Permission.getConsent(
+    activity: Activity
+): RequestOperation {
+    val stringPermissions = arrayOf(
+        this.identifier
+    )
+    return RequestOperation(
+        stringPermissions,
+        activity,
         null
     ).also { operation ->
         operation.start()
