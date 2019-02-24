83989d1f3dafbcf0464110ce5bc64c3608e8bf19
Igor Matyushkin <igormatyushkin014@gmail.com>
2019-02-25 00:18:57 +0300

    Suppressed deprecation warnings in RequestHolder

--- consent/src/main/java/com/visuality/consent/request/RequestHolder.kt
+++ consent/src/main/java/com/visuality/consent/request/RequestHolder.kt
@@ -1,6 +1,9 @@
 package com.visuality.consent.request
 
+import android.annotation.SuppressLint
+
 internal object RequestHolder {
 
+    @SuppressLint("StaticFieldLeak")
     var currentRequestOperation: RequestOperation? = null
 }
